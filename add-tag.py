c41ab41bfc6f6263a83a610ced295e73f7043008
jhinkey-iex <jim.hinkey@iexcloud.io>
2022-10-12 19:18:57 -0400

    TEMP html file

---
+++ add-tag.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+def main():
+    files = sorted(Path().rglob("*.html"))
+    for fname in files:
+        addTag(fname)
+    # fname = "developer-tools.html"
+    
+
+def addTag(fname):
+    delim = "</head>"
+    insert = "\n    <foo>\n      FOO\n    </foo>\n  "
+    file = open(fname, "rt")
+    str = file.read()
+    file.close()
+
+    head_split = str.split(delim, 1)
+    new_str = head_split[0] + insert + delim + head_split[1]
+    file = open(fname, "wt")
+    file.write(new_str)
+    file.close()
+
+if __name__ == "__main__":
+    main()
